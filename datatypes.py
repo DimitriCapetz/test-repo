492bb8ffc2d3ad12b2b5af8a81fa814c57255a39
Dimitri Capetz <dimitri.capetz@gmail.com>
2017-04-21 15:46:19 +0000

    Added datatypes script for demo.

---
+++ datatypes.py
@@ -0,0 +1,6 @@
+#Set a string variable, print and verify type.
+string1 = "Dogs are a man's best friend."
+print("The 'string1' variable = " + string1)
+type_string = str(type(string1))
+print("The 'string1' variable is " + type_string)
+print()
