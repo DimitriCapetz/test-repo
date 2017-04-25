823dd7af7b97117c7d7b13174b88ba5d39f7ccca
Dimitri Capetz <dimitri.capetz@gmail.com>
2017-04-25 18:00:48 +0000

    Finished loops and functions demo scripts.

--- functions.py
+++ functions.py
@@ -1,26 +1,57 @@
-#Loop for item indexes.
-tup1 = ("item1","item2","item3","item4","item5")
-print("ITEMS!")
-for item in tup1:
-    print(item)
+#Basic function definition and calling it.
+def function1():
+    print("I'm 'function1'.")
+    print()
+
+function1()
+
+#Functions can also have things (args) passed into them.
+def function2(arg1,arg2):
+    print("I'm 'function2' with 'arg1' of " + arg1)
+    print("Oh, I forgot!  'arg2' is " + arg2)
+
+function2("heavy","metal")
 print()
 
-#Now, let's define a dictinary of lists.
-games = {"PS4":["Horizon: Zero Dawn","Persona 5","Nier: Automata"],"XBox One":["Gears of War 4","Halo 5","ReCore"],"Nintendo Switch":["Breath of the Wild","Mario Kart 8","Snipperclips"]}
-#Parsing all these manually would be annoying...
-print(games["PS4"][0] + " is a Playstation 4 game.")
-print(games["PS4"][1] + " is a Playstation 4 game.")
-print(games["PS4"][2] + " is a Playstation 4 game.")
-print("Ahhhhhhh!")
+#Functions are great for things you do multiple times.
+print("interface Gi1/1")
+print("  switchport")
+print("  switchport mode access")
+print("  switchport access vlan 100")
+print("  switchport voice vlan 200")
+print("  speed 1000")
+print("  duplex full")
+print("  spanning-tree portfast")
+print("  no shutdown")
+print()
+print("interface Gi1/2")
+print("  switchport")
+print("  switchport mode access")
+print("  switchport access vlan 100")
+print("  switchport voice vlan 200")
+print("  speed 1000")
+print("  duplex full")
+print("  spanning-tree portfast")
+print("  no shutdown")
+print("...")
 print()
 
-#Let's simplify with a loop.
-for ps4releases in games["PS4"]:
-    print(ps4releases + " is a Playstation 4 game.")
-print("Much better!")
+#Now use the function for the same thing.
+def portconfig(port):
+    print("interface Gi1/" + str(port))
+    print("  switchport")
+    print("  switchport mode access")
+    print("  switchport access vlan 100")
+    print("  switchport voice vlan 200")
+    print("  speed 1000")
+    print("  duplex full")
+    print("  spanning-tree portfast")
+    print("  no shutdown")
+
+portconfig(1)
+portconfig(2)
 print()
 
-#Nest a loop to print them all!
-for platform in games:
-    for release in games[platform]:
-        print(release + " is a video game.")
+#Getting sik wit it.
+for x in range(1,10):
+    portconfig(x)
